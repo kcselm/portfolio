2495a6d4497a028aa30e90d95ea2955ccd285e0d
kcselm <kcselm93@gmail.com>
2024-09-11 21:13:17 -0700

    initial commit

---
+++ src/components/resume-card.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import BlurFade from "./magicui/blur-fade";
+
+const BLUR_FADE_DELAY = 0.04;
+
+interface ResumeCardProps {
+  logoUrl: string;
+  altText: string;
+  title: string;
+  subtitle?: string;
+  href?: string;
+  period: string;
+  description?: string;
+  skills?: string[];
+}
+export const ResumeCard = ({
+  logoUrl,
+  altText,
+  title,
+  subtitle,
+  href,
+  period,
+  description,
+  skills,
+}: ResumeCardProps) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (description) {
+      e.preventDefault();
+      setIsExpanded(!isExpanded);
+    }
+  };
+
+  return (
+    <Link
+      href={href || "#"}
+      target="_blank"
+      className="block cursor-pointer"
+      onClick={handleClick}
+    >
+      <Card className="flex">
+        <div className="flex-none">
+          <Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
+            <AvatarImage
+              src={logoUrl}
+              alt={altText}
+              className="object-contain"
+            />
+            <AvatarFallback>{altText[0]}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex-grow ml-4 items-center flex-col group">
+          <CardHeader>
+            <div className="flex items-center justify-between gap-x-2 text-base">
+              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+                {title}
+
+                <ChevronRightIcon
+                  className={cn(
+                    "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
+                    isExpanded ? "rotate-90" : "rotate-0"
+                  )}
+                />
+              </h3>
+              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+                {period}
+              </div>
+            </div>
+            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
+          </CardHeader>
+          {description && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{
+                opacity: isExpanded ? 1 : 0,
+
+                height: isExpanded ? "auto" : 0,
+              }}
+              transition={{
+                duration: 0.7,
+                ease: [0.16, 1, 0.3, 1],
+              }}
+              className="mt-2 text-xs sm:text-sm"
+            >
+              {description}
+              <div className="flex gap-1 mt-3">
+                {skills &&
+                  skills.map((skill, id) => (
+                    <BlurFade
+                      key={skill}
+                      delay={BLUR_FADE_DELAY * 10 + id * 0.05}
+                    >
+                      <Badge key={skill}>{skill}</Badge>
+                    </BlurFade>
+                  ))}
+              </div>
+            </motion.div>
+          )}
+        </div>
+      </Card>
+    </Link>
+  );
+};
---
+++ src/data/resume.tsx
@@ -0,0 +1,328 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Kevin Selm",
+  initials: "KS",
+  url: "https://kcselm.io",
+  location: "Portland, OR",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Full Stack Web Developer who loves learning and building things ",
+  summary:
+    "I'm a Full Stack TypeScript Developer who loves turning ideas into awesome software solutions. I'm all about lifelong learning and keeping up with new tech. Outside of work I still like diving into personal projects to keep my skills sharp and my curiosity happy. I love networking and meeting new people, so lets [connect!](/#contact) and chat soon!",
+  avatarUrl: "me.png",
+  skills: [
+    "HTML",
+    "CSS",
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "C#",
+    "Java",
+    "SQL",
+    "Postgres",
+    "Docker",
+    "Azure",
+    "git",
+  ],
+  simple_icon_slugs: [
+    "typescript",
+    "javascript",
+    "java",
+    "react",
+    "redux",
+    "html5",
+    "css3",
+    "tailwindcss",
+    "nodedotjs",
+    "prisma",
+    "express",
+    "nextdotjs",
+    "csharp",
+    "microsoftazure",
+    "powerbi",
+    "python",
+    "django",
+    "mysql",
+    "postgresql",
+    "trpc",
+    "vercel",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "gitlab",
+    "mongodb",
+    "visualstudiocode",
+    "webpack",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "kcselm93@gmail.com",
+    tel: "8122128015",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://dub.sh/kselm-github",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://dub.sh/kselm-linkedin",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "",
+        icon: Icons.x,
+
+        navbar: false,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "",
+        icon: Icons.youtube,
+        navbar: false,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:kcselm93@gmail.com",
+        icon: Icons.email,
+        navbar: true,
+      },
+    },
+  },
+  showcaseImages: [
+    { imageUrl: "/showcase/dog.png", altText: "Me and my dog hamocking" },
+    {
+      imageUrl: "/showcase/discgolf.png",
+      altText: "Playing disc golf",
+    },
+    {
+      imageUrl: "/showcase/me-cascades.jpg",
+      altText: "At North Cascades National Park",
+    },
+    {
+      imageUrl: "/showcase/rock-climbing.png",
+      altText: "Bouldering gym",
+    },
+    {
+      imageUrl: "/showcase/wife.png",
+      altText: "Engagement photo with my wife",
+    },
+    { imageUrl: "/showcase/coffee.jpg", altText: "coffee shop" },
+    {
+      imageUrl: "/showcase/ultimate.jpg",
+      altText: "Playing club ultimate frisbee",
+    },
+  ],
+
+  work: [
+    {
+      company: "Tyler Technologies",
+      href: "https://www.tylertech.com/",
+      location: "Remote",
+      title: "Frontend Software Developer",
+      logoUrl: "/tyler-technologies.svg",
+      start: "May 2024",
+      end: "Current",
+      description:
+        "Developing WCAG-compliant web pages and custom reusable components for clients servicing millions of users, significantly enhancing inclusivity and usability.",
+      skills: ["Typescript", "HTML", "CSS"],
+    },
+    {
+      company: "Leidos",
+      href: "https://www.leidos.com/",
+      location: "Remote",
+      title: "Frontend Software Developer",
+      logoUrl: "/leidos.svg",
+      start: "June 2023",
+      end: "December 2023",
+      description:
+        "Led frontend development for a web application that provided admins and team leads with a customizable dashboard to visualize team data and metrics.",
+      skills: ["React", "Typescript", "HTML", "CSS"],
+    },
+    {
+      company: "Sur-Seal",
+      href: "https://www.sur-seal.com/",
+      location: "Remote",
+      title: "Fullstack Software Developer",
+      logoUrl: "/sur-seal.svg",
+      start: "February 2021",
+      end: "June 2023",
+      description:
+        "Fullstack developer responsible for maintaining and enhancing in-house sales and product application, creating data visualizations and analytical tools, and automating reporting processes.",
+      skills: ["C#", "React", "Javacript", "Python", "PowerBI", "SQL"],
+    },
+    {
+      company: "TDS Telecom",
+      href: "https://www.tdstelecom.com/",
+      location: "Remote",
+      title: "Software Engineer 1",
+      logoUrl: "/tds.png",
+      start: "January 2019",
+      end: "February 2021",
+      description:
+        "Backend network application developer working on provisioning software and network inventory management applications.",
+      skills: ["Python", "Java", "SQL", "Bash scripting", "Perl"],
+    },
+    {
+      company: "TDS Telecom ",
+      href: "https://www.tdstelecom.com/",
+      location: "Remote",
+      title: "Business Risk Intelligence Intern",
+      logoUrl: "/tds.png",
+      start: "April 2018",
+      end: "January 2018",
+      description:
+        "Worked with senior developers and accountants on data analysis and frontend web development projects",
+      skills: ["React", "Angular", "Javascript", "Python"],
+    },
+  ],
+  education: [
+    {
+      school: "Madison Area Technical College",
+      href: "https://madisoncollege.edu/",
+      degree: "Associate's Degree Web Software Development",
+      logoUrl: "/matc-dark.png",
+      start: "2018",
+      end: "2019",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+};
