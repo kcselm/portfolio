4812af918b8a6043bdca801b17f84448cff179d3
kcselm <kcselm93@gmail.com>
2025-07-16 17:49:05 -0700

    Updating resume and projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import BlurFade from "@/components/magicui/blur-fade";
-import BlurFadeText from "@/components/magicui/blur-fade-text";
-import IconCloud from "@/components/magicui/icon-cloud";
-import { ResumeCard } from "@/components/resume-card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
-import { DATA } from "@/data/resume";
-import Link from "next/link";
-import Markdown from "react-markdown";
+import BlurFade from "@/components/magicui/blur-fade"
+import BlurFadeText from "@/components/magicui/blur-fade-text"
+import IconCloud from "@/components/magicui/icon-cloud"
+import { ProjectCard } from "@/components/project-card"
+import { ResumeCard } from "@/components/resume-card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { DATA } from "@/data/resume"
+import Link from "next/link"
+import Markdown from "react-markdown"
 
-const BLUR_FADE_DELAY = 0.04;
+const BLUR_FADE_DELAY = 0.04
 
 export default function Page() {
   return (
@@ -122,8 +123,44 @@ export default function Page() {
             ))}
           </div>
         </div>
-        <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background px-28 pb-20 pt-8 ">
+        <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg px-28 pb-20 pt-8 ">
           <IconCloud iconSlugs={DATA.simple_icon_slugs} />
+        </div>
+      </section>
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here's my latest work:
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
         </div>
       </section>
       <section id="contact">
@@ -161,5 +198,5 @@ export default function Page() {
         </div>
       </section>
     </main>
-  );
+  )
 }
